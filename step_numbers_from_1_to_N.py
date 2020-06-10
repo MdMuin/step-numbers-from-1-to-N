2951996aa1c04b6644a2fb0670131ba8f1e39cab
MdMuin <48877515+MdMuin@users.noreply.github.com>
2020-06-10 16:24:55 +0530

    Add files via upload

---
+++ step_numbers_from_1_to_N.py
@@ -0,0 +1,12 @@
+a=int(input("enter num:"));p=0
+for i in range(10,a+1):
+    d,c=i,0
+    while d>9:
+        if abs(d%10-(d//10)%10)!=1:
+            c=1
+            break
+        d//=10;
+    if c==0:print(i,end=' ');p=1
+if p==0:print(-1)
+        
+            
